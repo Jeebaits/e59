2fa5ccbeaf296589e4f5691c98eeff8a29388f00
jeebaits <jeraldlinkedin@gmail.com>
2024-02-13 21:20:47 +0800

    useState, prev state, event, handleaddGoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 import goalsImg from "./assets/react.svg";
+import { useState } from "react";
 
+type CourseGoal = {
+  title: string,
+  description: string,
+  id: number
+}
 const App = () => {
+  const [goals, setGoals] = useState<CourseGoal[]>([]); // initialized to empty array with type CourseGoal
+  const handleAddGoal = () => {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: 'Learn Vite + React-TS',
+        description: 'from zero to hero'
+      }
+      return [...prevGoals, newGoal]; // add to previous list
+    });
+  }
   return (
     <main>
       <Header image={{src: goalsImg, alt: 'image of goals'}}>
         <h1>Your Course Goals:</h1>
       </Header>
-      <CourseGoal title="Learn Vite + React-TS" description="from zero to hero">
-        <p>Author: JRamos</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      {/* create list of goals */}
+      <ul>
+        {goals.map((goal) => (
+          <li>
+          <CourseGoal title={goal.title} description={goal.description}>
+            <p>Author: JRamos</p>
+          </CourseGoal>
+        </li>
+        ))}
+      </ul>
     </main>
   )
 }
